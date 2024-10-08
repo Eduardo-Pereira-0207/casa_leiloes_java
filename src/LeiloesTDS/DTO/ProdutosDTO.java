3c6561d9ec376e4a667e03f9aa1f1a8f93092f55
X-edw-X <129557391+Eduardo-Pereira-0207@users.noreply.github.com>
2024-10-08 17:27:17 -0300

    Implementanção da listagem de produtos Vendidos e Cadastrados

--- src/LeiloesTDS/DTO/ProdutosDTO.java
+++ src/LeiloesTDS/DTO/ProdutosDTO.java
@@ -4,7 +4,6 @@ package LeiloesTDS.DTO;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Adm
@@ -34,12 +33,6 @@ public class ProdutosDTO {
 
     public void setId(Integer id) {
         this.id = id;
-        
-        
-        
-        
-        
-        
     }
 
     public void setNome(String nome) {
